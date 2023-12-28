c617de5fc50e88e043f698dc9206b55b66e86c77
steelsnake93 <nikolanikotasev@protonmail.com>
2023-12-28 09:48:17 +0100

    Adding Metadata: Page title and descriptions.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'; // remove fetchRevenue
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
  
 export default async function Page() {
 
